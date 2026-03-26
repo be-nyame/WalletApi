7fb6ecf2435f33a23053ed3c23c6af95f04c92f5
be-nyame <benessilfie60@yahoo.com>
2026-03-26 15:21:38 +0000

    Add registration request validation

---
+++ WalletApi.Application/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using WalletApi.Application.DTOs.Auth;
+
+namespace WalletApi.Application.Validators;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(256);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .Matches("[A-Z]").WithMessage("Password must contain an uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain a lowercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain a number.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain a special character.");
+
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
+    }
+}
